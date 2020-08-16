a768b11fe16256ae7e43e1652b897aa669cab06a
Conor <31479541+cbyrne@users.noreply.github.com>
2020-08-16 09:44:48 +0100

    More debug changes

--- craftlib-chat/src/main/kotlin/dev/zerite/craftlib/chat/component/ChatComponentTypeAdapter.kt
+++ craftlib-chat/src/main/kotlin/dev/zerite/craftlib/chat/component/ChatComponentTypeAdapter.kt
@@ -62,11 +62,14 @@ class ChatComponentTypeAdapter : TypeAdapter<BaseChatComponent>() {
             write("action", it.action.name.toLowerCase())
             write("value", it.value)
         }
-        out.write("hoverEvent", input.hoverEvent) {
-            write("action", it.action.name.toLowerCase())
-            name(hoverReadValue)
-            write(out, it.value)
+        if(hoverReadValue != "contents") {
+            out.write("hoverEvent", input.hoverEvent) {
+                write("action", it.action.name.toLowerCase())
+                name("value")
+                write(out, it.value)
+            }
         }
+
 
         out.writeArray("extra", input.siblings.toTypedArray()) { write(out, it) }
 
@@ -148,11 +151,13 @@ class ChatComponentTypeAdapter : TypeAdapter<BaseChatComponent>() {
                 )
             }
 
-            this["hoverEvent"]?.asJsonObject?.let {
-                base.hoverEvent = HoverEvent(
-                    HoverEvent.Action.valueOf(it["action"].asString.toUpperCase()),
-                    it[hoverReadValue].asComponent()
-                )
+            if(hoverReadValue != "contents") {
+                this["hoverEvent"]?.asJsonObject?.let {
+                    base.hoverEvent = HoverEvent(
+                        HoverEvent.Action.valueOf(it["action"].asString.toUpperCase()),
+                        it["value"].asComponent()
+                    )
+                }
             }
 
             this["extra"]?.asJsonArray?.takeIf { it.size() > 0 }
